36bd2b2cce71f220527ae02ef87a4de929bb6d14
Nakul Kumar <nakul.kumar@aiprus.com>
2026-05-13 15:35:07 +0530

    All is well

--- core/api/apiClient.js
+++ core/api/apiClient.js
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const BASE_URL = "http://192.168.31.114:3004/api";
+const BASE_URL = "https://talentboard-x-api-with-mongo-db.onrender.com/api";
 
 const apiClient = {
   getToken: async () => {
